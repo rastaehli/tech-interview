4810375ad158c76f2643bdfa3e46cdf6bef91fbe
rstaehli <rstaehli@acm.org>
2016-07-19 12:01:52 -0700

    fixed project review issues for questions 1 2 and 3

--- solutions.py
+++ solutions.py
@@ -39,11 +39,22 @@ class Node(object):
         self.visited = False
 
     def __str__(self):
-        return str(self.value)
+        return 'Node:'+str(self.value)
         # + ' edges: ' + str(self.edges) + ' visited: ' + str(self.visited)
 
     def __repr__(self):
-        return 'Node('+self.__str__()+')'
+        return 'Node('+self.__str__()+'['+str(self.edges)+'])'
+
+    def equals(self, node):
+        return self.value == node.value
+
+    def addEdge(self, edge):
+        otherNode = other(self, edge)  # get other node from edge
+        for e in self.edges:
+            if edge.value == e.value and otherNode == e.node_to:
+                return  e  # edge already added
+        newEdge = Edge(edge.value, self, otherNode)
+        self.edges.append(newEdge)
 
 class Edge(object):
     def __init__(self, value, node_from, node_to):
@@ -62,12 +73,6 @@ class Graph(object):
         self.nodes = nodes
         self.edges = edges
 
-    def edgesSum(self):
-        sum = 0
-        for e in self.edges:
-            sum += e.value
-        return sum
-
 class Partition(Graph):
     """constructing a partition, must take care to copy input nodes and
     edges, so this algorithm does not modify the original graph while
@@ -80,16 +85,17 @@ class Partition(Graph):
         self.edges = []
         self.nodeMap = {}
         for n in i_nodes:
-            self.addNode(self.copyNode(n))
+            self.addNode(n)
         for e in i_edges:
-            self.addEdge(self.copyEdge(e))
+            self.addEdge(e)
 
     def copyNode(self, node):
         return Node(node.value)
 
     def addNode(self, node):
-        self.nodes.append(node)
-        self.remember(node)
+        copy = self.copyNode(node)
+        self.nodes.append(copy)
+        self.remember(copy)
 
     def remember(self, node):
         """support lookup by value"""
@@ -100,7 +106,7 @@ class Partition(Graph):
             self.nodes.append(n)
         for e in partition.edges:
             self.edges.append(e)
-        self.addEdge(self.copyEdge(edge))
+        self.addEdge(edge)
 
     def copyEdge(self, edge):
         f = self.lookup(edge.node_from)
@@ -108,11 +114,10 @@ class Partition(Graph):
         return Edge(edge.value, f, t)
 
     def addEdge(self, edge):
-        f = self.lookup(edge.node_from)
-        t = self.lookup(edge.node_to)
-        f.edges.append(edge)
-        t.edges.append(edge)
-        self.edges.append(edge)
+        copy = self.copyEdge(edge)
+        copy.node_from.addEdge(copy)
+        copy.node_to.addEdge(copy)
+        self.edges.append(copy)
 
     def lookup(self, node):
         return self.nodeMap[node.value]
@@ -124,6 +129,8 @@ class Partition(Graph):
         s = ''
         for n in self.nodes:
             s += str(n) + ', '
+        for e in self.edges:
+            s += str(e.value) + ', '
         return s
 
     def __repr__(self):
@@ -184,6 +191,35 @@ def findPartition(edge, newNode, partitions):
             return p
     return None
 
+def other(node, edge):
+    if edge.node_from.equals(node):
+        return edge.node_to
+    return edge.node_from
+
+def adjacencyList(tree):
+    aListMap = {}
+    for n in tree.nodes:
+        l = []
+        for e in n.edges:
+            l.append((other(n,e).value, e.value))
+        aListMap[n.value] = l
+    return aListMap
+
+def sumEdges(adjacencyListMap):
+    edgeValues = {}
+    for n in adjacencyListMap:
+        for tuple in adjacencyListMap[n]:
+            # non-directional edge id: node names in lexical order
+            if tuple[0] < n:  # node value sort order
+                edgeId = tuple[0]+'-'+n
+            else:
+                edgeId = n+'-'+tuple[0]
+            edgeValues[edgeId] = tuple[1]
+    sum = 0
+    for edgeId in edgeValues:
+        sum += edgeValues[edgeId]
+    return sum
+
 # ======== solution functions =====================================
 
 
@@ -195,7 +231,6 @@ def question1(s, t):
         return True
     candidateStart = -1
     lastPossibleStart = len(s) - len(t)
-    # print('candidateStart {} < lastPossibleStart {}'.format(candidateStart, lastPossibleStart))
     while candidateStart < lastPossibleStart:
         candidateStart += 1
         if anagramFound(s, t, candidateStart):
@@ -203,7 +238,7 @@ def question1(s, t):
     return False
 
 
-def question2(a):
+def question2FirstTry(a):
     aLength = len(a)
     if aLength <= 0:
         return ''
@@ -234,7 +269,6 @@ def question2(a):
             if canGrow(p, aLength) and (a[p.start-1] == a[p.start + p.length]):
                 p.start -= 1
                 p.length += 2
-                # print(p.start, p.length)
                 if longest.length < p.length:
                     longest = p
             else:
@@ -242,8 +276,18 @@ def question2(a):
             p = p.next
 
     # now longest is last long Palindrome seen and no more to explore
-    # print(longest.start, longest.length)
     return a[longest.start:longest.start+longest.length]
+
+
+def question2(a):
+    aLength = len(a)
+    l = aLength
+    while l > 0:
+        for start in range(0, aLength - (l-1)):
+            if a[start:start+l:1] == a[start+l-1:None if (start-1)<0 else (start-1):-1]:
+                return a[start:start+l:1]
+        l -= 1
+    return ''
 
 
 def question3(G):
@@ -262,8 +306,8 @@ def question3(G):
                 nodes.append(nmap[id2])
             edge = Edge(weight, nmap[id1], nmap[id2])
             edges.append(edge)
-            nmap[id1].edges.append(edge)
-            nmap[id2].edges.append(edge)
+            nmap[id1].addEdge(edge)
+            nmap[id2].addEdge(edge)
 
     graph = Graph(nodes, edges)
     #handle trivial case of graph with one or fewer nodes
@@ -301,79 +345,53 @@ def question3(G):
         else:
             toMerge[0].addPartitionWithEdge(toMerge[1], e)
             partitions.remove(toMerge[1])
-
-    return partitions[0]
+    return adjacencyList(partitions[0])
 
 
 # ========= test cases =============================================
 
 # ========= question1 ========
-print("question1('', '')")
-print("expect: True")
-print("question1('', '')")
-print("expect: True")
-print("question1('', 'a')")
-print("expect: False")
-print("question1('b', 'b')")
-print("expect: True")
-print("question1('abc', 'b')")
-print("expect: True")
-print("question1('abc', 'cb')")
-print("expect: True")
-print("question1('abc', 'cab')")
-print("expect: True")
-print("question1('abcbcbbbc', 'aca')")
-print("expect: False")
+assert question1('', '') == True
+assert question1('', 'a') == False
+assert question1('b', 'b') == True
+assert question1('abc', 'b') == True
+assert question1('abc', 'cb') == True
+assert question1('abc', 'cab') == True
+assert question1('abcbcbbbc', 'aca') == False
 
 # ========= question2 ========
 
-print("question2('')")
-print("expect: ''")
-print("question2('a')")
-print("expect: 'a'")
-print("question2('b')")
-print("expect: 'b'")
-print("question2('abc')")
-print("expect: 'a'")
-print("question2('abbc')")
-print("expect: 'bb'")
-print("question2('abbbc')")
-print("expect: 'bbb'")
-print("question2('abccb')")
-print("expect: 'bccb'")
-print("question2('bccba')")
-print("expect: 'bccb'")
-print("question2('abcdcb')")
-print("expect: 'bcdcb'")
-print("question2('bcdcba')")
-print("expect: 'bcdcb'")
-print("question2('abcdcdccdedccbb')")
-print("expect: 'ccdedcc'")
-print("question2('abcdefe')")
-print("expect: 'efe'")
+assert question2('') == ''
+assert question2('a') == 'a'
+assert question2('b') == 'b'
+assert question2('abc') == 'a'
+assert question2('abbc') == 'bb'
+assert question2('abbbc') == 'bbb'
+assert question2('abccb') == 'bccb'
+assert question2('bccba') == 'bccb'
+assert question2('abcdcb') == 'bcdcb'
+assert question2('bcdcba') == 'bcdcb'
+assert question2('abcdcdccdedccbb') == 'ccdedcc'
+assert question2('abcdefe') == 'efe'
 
 # ========= question3 ========
 
-def test(g, expect):
-    print(g)
-    print('expect {}, actual {}'.format(expect, question3(g).edgesSum()))
+# g = {'A': [('B', 2)],
+#  'B': [('A', 2), ('C', 5)],
+#  'C': [('B', 5)]}
+# mst = question3(g)
+# assert sumEdges(mst) == 7
 
-print("g = {'A': [('B', 2)],")
-print(" 'B': [('A', 2), ('C', 5)],")
-print(" 'C': [('B', 5)]}")
-print("question3(g).edgesSum()")
-print("expect: 7")
-
-print("g = {")
-print("    'A': [('B', 3), ('F', 5), ('C', 2)],")
-print("    'B': [('A', 3), ('C', 4), ('D', 1)],")
-print("    'C': [('A', 2), ('B', 4), ('E', 1)],")
-print("    'D': [('B', 1), ('E', 5), ('F', 1)],")
-print("    'E': [('C', 1), ('D', 5), ('F', 1)],")
-print("    'F': [('E', 1), ('A', 5), ('D', 1)],")
-print("}")
-print("question3(g).edgesSum()")
-print("expect: 6")
+g = {
+    'A': [('B', 3), ('F', 5), ('C', 2)],
+    'B': [('A', 3), ('C', 4), ('D', 1)],
+    'C': [('A', 2), ('B', 4), ('E', 1)],
+    'D': [('B', 1), ('E', 5), ('F', 1)],
+    'E': [('C', 1), ('D', 5), ('F', 1)],
+    'F': [('E', 1), ('A', 5), ('D', 1)],
+}
+mst = question3(g)
+assert sumEdges(mst) == 6
 
 # ========= question4 ========
 
